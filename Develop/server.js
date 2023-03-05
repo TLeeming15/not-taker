ccec15b6d80944061fea42ef8cb40895c9548b3c
TLeeming15 <tyler.leeming21@gmail.com>
2023-03-05 14:39:01 +1030

    server.js work

---
+++ Develop/server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const path = require('path');
+let notes = require("./db/db.json")
+
+const app = express();
+const PORT = 3001;
+const ROOT = { root: path.join(__dirname, './public') };
+
+
+app.use(express.static(path.join(__dirname, './public')))
+
+app.get('/notes', (req, res) =>
+  res.sendFile('notes.html', ROOT)
+);
+app.get('/api/notes', (req, res) => {
+  res.json(notes)
+});
+app.post('/api/notes', (req, res) => {
+
+  notes.push(req.body)
+
+        // Write updated reviews back to the file
+        fs.writeFile('./db/db.json',JSON.stringify(notes),
+          (writeErr) =>
+            writeErr
+              ? console.error(writeErr)
+              : console.info('Successfully updated reviews!')
+        );
+
+  res.json(notes)
+});
+
+app.get('*', (req, res) =>
+res.sendFile('index.html', ROOT)
+);
+
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
