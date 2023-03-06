6c0b5c0bd42d3ec5b9ca60baa9c1a64cd98dd1f6
TLeeming15 <tyler.leeming21@gmail.com>
2023-03-06 18:47:48 +1030

    bug fix

--- Develop/server.js
+++ Develop/server.js
@@ -7,6 +7,7 @@ const app = express();
 const PORT = 3001;
 const ROOT = { root: path.join(__dirname, './public') };
 
+app.use(express.json());
 
 app.use(express.static(path.join(__dirname, './public')))
 
