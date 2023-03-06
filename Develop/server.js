6b2aca7781647129b1cc777e3f1ec25a1e85a63b
TLeeming15 <tyler.leeming21@gmail.com>
2023-03-06 18:12:32 +1030

    fs update

--- Develop/server.js
+++ Develop/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 let notes = require("./db/db.json")
 
 const app = express();
@@ -12,20 +13,22 @@ app.use(express.static(path.join(__dirname, './public')))
 app.get('/notes', (req, res) =>
   res.sendFile('notes.html', ROOT)
 );
+
 app.get('/api/notes', (req, res) => {
   res.json(notes)
 });
+
 app.post('/api/notes', (req, res) => {
 
   notes.push(req.body)
 
-        // Write updated reviews back to the file
-        fs.writeFile('./db/db.json',JSON.stringify(notes),
-          (writeErr) =>
-            writeErr
-              ? console.error(writeErr)
-              : console.info('Successfully updated reviews!')
-        );
+  // Write updated notes back to the file
+  fs.writeFile('./db/db.json',JSON.stringify(notes),
+    (writeErr) =>
+      writeErr
+        ? console.error(writeErr)
+        : console.info('Success!')
+  );
 
   res.json(notes)
 });
