23ceff485af21ae57f2a664fda3eb9c45bb6ae3e
TLeeming15 <tyler.leeming21@gmail.com>
2023-03-09 18:12:25 +1030

    debug

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 let notes = require("./db/db.json")
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const ROOT = { root: path.join(__dirname, './public') };
 
 app.use(express.json());
